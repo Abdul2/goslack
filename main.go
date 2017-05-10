a01eead576712e49dfcc1297547adb4e0347449b
abdul abdi <abdulrashid2@googlemail.com>
2017-05-10 10:18:34 +0100

    Update main.go

--- main.go
+++ main.go
@@ -7,15 +7,15 @@ import (
 	"os"
 )
 
-
-
 const (
 
 	channelName = "random"
 )
-
+//send messages to a channel
 func main() {
 
+	
+	//to obtain a token go to this site :- https://api.slack.com/custom-integrations/legacy-tokens
 	token := os.Getenv("hoddspslacktoken")
 	api := slack.New(token)
 	err := api.ChatPostMessage(channelName, "hi slackers - test message", nil)
